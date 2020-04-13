4f7b956050f9e4a99347df3f4bba366c3f716602
Arafat Najjashi <arafath.moghul@gmail.com>
2020-04-13 21:49:24 +0530

    output commit

--- index.js
+++ index.js
@@ -33,6 +33,23 @@ const url = require('url');
 
 /////////////////////////////////////
 // SERVER 
+const replaceTemplate = (temp,product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/(%QUANTITY%)/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+    
+    if(!product.organic){
+    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    }
+    return output;
+
+};
+
 const templateOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8');
 const templateCard = fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8');
 const templateProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`,'utf-8');
@@ -48,7 +65,9 @@ const server = http.createServer((req, res) => {
     //OVERVIEW PAGE
     if(pathName === '/' || pathName === '/overview'){
         res.writeHead(200 , {'Content-type' : 'text/html'});
-        res.end(templateOverview);
+        const cardsHtml = dataObj.map(el => replaceTemplate(templateCard, el)).join('');
+        const output = templateOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+        res.end(output);
     
     //PRODUCT PAGE
     }else if(pathName ==='/product'){
