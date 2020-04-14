920d8efbfe41e22ae7330f0a056bb3bf483f74f1
Arafat Najjashi <arafath.moghul@gmail.com>
2020-04-14 21:28:51 +0530

    products added

--- index.js
+++ index.js
@@ -60,22 +60,24 @@ const dataObj = JSON.parse(data);
 
 
 const server = http.createServer((req, res) => {
-    const pathName = req.url;
-
+    
+    const {query , pathname } = url.parse(req.url,true);
     //OVERVIEW PAGE
-    if(pathName === '/' || pathName === '/overview'){
+    if(pathname === '/' || pathname === '/overview'){
         res.writeHead(200 , {'Content-type' : 'text/html'});
         const cardsHtml = dataObj.map(el => replaceTemplate(templateCard, el)).join('');
         const output = templateOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
         res.end(output);
     
     //PRODUCT PAGE
-    }else if(pathName ==='/product'){
-
-        res.end('This is the Product');
+    }else if(pathname ==='/product'){
+        res.writeHead(200 , {'Content-type' : 'text/html'});
+        const product = dataObj[query.id];
+        const output = replaceTemplate(templateProduct, product);
+        res.end(output);
 
     //API
-    }else if(pathName === '/API'){
+    }else if(pathname === '/API'){
        res.writeHead(200, {'Content-type' : 'application/json'});
         res.end(data);
 
